7078c9d9aa1d142090164fa1e6a06c3217791f50
bobanum <mboudrea@cstj.qc.ca>
2018-01-02 13:21:34 -0500

    Amélioration de serve.bat

--- serve.7060.bat
+++ serve.7060.bat
@@ -4,7 +4,7 @@ set fic=%~nx0
 set chemin=%~dp0
 
 FOR /F "tokens=2,3 delims=." %%I IN ("%fic%") DO (
-    if not "%%i"=="bat" (
+    if "%%I"=="bat" (
 		set port=8000
 	) else (
 		set port=%%I
@@ -13,15 +13,13 @@ FOR /F "tokens=2,3 delims=." %%I IN ("%fic%") DO (
 
 cd %chemin%
 :checkport
-netstat -an |find "%port%" >nul
-if ERRORLEVEL 1 (
-	rem "pas pris"
-) else (
-	echo "Le port '%port%' est pris."
+netstat -oan |findstr /c:":%port% " >nul
+if %ERRORLEVEL% == 0 (
+	echo Le port '%port%' est pris.
 	set /A port=port+1
 	goto checkport
 )
+echo On utilise le port '%port%'.
 
-rem brackets . &
-start "" http://localhost:%port% &
-C:\wamp64\bin\php\php7.0.10\php.exe -S 0.0.0.0:%port%
+start "" http://localhost:%port%
+php.exe -S 0.0.0.0:%port%
